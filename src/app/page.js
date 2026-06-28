1dc4242291b3cd013c0582ed485603074253d14c
SM-Raihan77 <smraihan720@gmail.com>
2026-06-29 00:07:12 +0600

    Complete homepage layout

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,16 @@
 import Image from "next/image";
 import BloodDonationRequests from "./components/BloodDonationRequests";
 import BloodSearchPage from "./components/BloodSearchPage";
-
+import Banner from "./components/Banner";
+import StatsBar from "./components/StatsBar";
+import ExtraBannerSection from "./components/ExtraBannerSection";
 export default function Home() {
   return (
   <div>
+    <Banner />
+    <StatsBar />
     <BloodDonationRequests />
- <BloodSearchPage/>
+    <ExtraBannerSection />
   </div>
   );
 }
